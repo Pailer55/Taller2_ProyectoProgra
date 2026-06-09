3a80dfe865036a3407f59ceacefbfd9e99ad32cb
Pailer55 <elias.jamesc.55@gmail.com>
2026-06-08 23:43:37 -0400

    fix: character movement fixed

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
-    [Header("Movimiento")]
+    
     public float velocidadLateral = 5f;
     public float velocidadAdelante = 5f;
     public float fuerzaSalto = 7f;
-    private bool estaEnSuelo = true;
+    public bool estaEnSuelo = true;
+    public Transform limiteIzq;
+    public Transform limiteDer;
 
     private Rigidbody rb;
 
@@ -20,16 +22,17 @@ public class MovimientoPersonaje : MonoBehaviour
     void Update()
     {
         MoverLateral();
-        Saltar();
+        Saltar();        
     }
 
     void Saltar()
-{
-    if (Input.GetKeyDown(KeyCode.Space) && estaEnSuelo)
     {
-        rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && estaEnSuelo)
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+            estaEnSuelo = false;
+        }
     }
-}
 
     void OnCollisionEnter(Collision collision)
     {
@@ -54,5 +57,17 @@ public class MovimientoPersonaje : MonoBehaviour
         // Si no presiona nada, detiene el movimiento lateral
         rb.velocity = new Vector3(0, rb.velocity.y, velocidadAdelante);
         }
+
+          Vector3 pos = transform.position;
+    float clampedX = Mathf.Clamp(pos.x, limiteIzq.position.x, limiteDer.position.x);
+
+    // Si toco el limite, anula la velocidad en X
+    if (pos.x != clampedX)
+    {
+        rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+    }
+
+    pos.x = clampedX;
+    transform.position = pos;
     }
 }
