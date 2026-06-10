801d1302353d13940faa00f3b88f5907559f0f23
Pailer55 <elias.jamesc.55@gmail.com>
2026-06-10 01:12:59 -0400

    feat: car test added

---
+++ Assets/Scripts/Vehiculo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vehiculo : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bala"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
